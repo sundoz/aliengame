1df552f941ff2290a8d94cac9c5878a98c9b52c4
sundoz <sundoz@mail.ru>
2021-01-28 23:00:28 +0300

    alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,24 +3,29 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+
 import game_functions as gf
 
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_wigth, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
-    bullets = Group()
-    while True:
 
+    bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     run_game()
 
--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 class Settings():
     """A class to store all settings for Alien Invasion."""
+    screen_width: int
 
     def __init__(self):
         """Initialize the game's settings."""
-        self.screen_wigth = 1200
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 200, 200)
         self.ship_speed_factor = 1.5
